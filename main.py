140991536aa2f8fffbf037b7e9f548d9849bec01
Кучин Денис <danielseddian@yandex.ru>
2021-03-20 14:52:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
 
+
+
+
+class Thing():
+
+    def __init__(self, name, health, added_defense, added_attack):
+        self.thing_name = name
+        self.health = health
+        self.added_defense = added_defense
+        self.added_attack = added_attack
+
+class Person():
+
+    def __init__(self, person_name, person_health, base_defense, base_attack):
+        self.person_name = person_name
+        self.person_health = person_health
+        self.base_defense = base_defense
+        self.base_attack = base_attack
+
+    def set_things(things):
+        
+
+    def take_damage():
+        
+    def make_damage():
+
+
+class Paladind(Person):
+
+    def __init__(self):
+        self.health *= 2
+        self.defense *= 2
+
+
+class Warrior(Person):
+
+    def __init__(self):
+        self.attack *= 2
+
+
+if __name__ == '__main__':
+
