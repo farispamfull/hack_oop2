8a497d632863328d71be83ec9a3995545a1edd13
Кучин Денис <danielseddian@yandex.ru>
2021-03-20 15:47:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,41 +2,52 @@
 
 
 
-class Thing():
+class Thing:
 
-    def __init__(self, name, health, added_defense, added_attack):
-        self.thing_name = name
-        self.health = health
-        self.added_defense = added_defense
-        self.added_attack = added_attack
+    def __init__(self, name, health=None, defense=None, attack=None):
+        self.thing_name = name 
+        self.added_health = 0 if health is None else health
+        self.added_defense = 0 if defense is None else defense
+        self.added_attack = 0 if attack is None else attack
+
 
 class Person():
 
-    def __init__(self, person_name, person_health, base_defense, base_attack):
-        self.person_name = person_name
-        self.person_health = person_health
-        self.base_defense = base_defense
-        self.base_attack = base_attack
+    def __init__(self, name, health, defense, attack):
+        self.person_name = name
+        self.base_health = health
+        self.base_defense = 30 if defense >= 30 else defense
+        self.base_attack = attack
 
     def set_things(things):
-        
+        pass
 
     def take_damage():
+        pass
         
     def make_damage():
+        pass
 
 
 class Paladind(Person):
 
-    def __init__(self):
-        self.health *= 2
-        self.defense *= 2
+    def __init__(self, health, defense):
+        self.health = health * 2
+        self.defense = defense * 2
 
 
 class Warrior(Person):
 
-    def __init__(self):
-        self.attack *= 2
+    def __init__(self, attack):
+        self.attack = attack * 2
+
+
+class Wizard(Person):
+
+    def __init__(self, health, defense, attack):
+        self.health = health / 2
+        self.defense = defense / 2
+        self.attack = attack * 4
 
 
 if __name__ == '__main__':
